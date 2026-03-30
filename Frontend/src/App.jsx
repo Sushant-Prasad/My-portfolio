cb1329591dfe52046356db3e27bc88177e45f1c4
Sushant-Prasad <sushantprasad77@gmail.com>
2026-03-30 23:35:41 +0530

    Add avatar image to assets

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import CustomCursor from "./components/customCursor";
 import Navbar from "./components/Navbar";
-import ParticlesBackground from "./components/ParticlesBackground";
+// import ParticlesBackground from "./components/ParticlesBackground";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Experience from "./pages/Experience";
@@ -13,7 +13,7 @@ function App() {
   return (
       <div className="relative gradient text-white">
         <CustomCursor/>
-        <ParticlesBackground/>
+        {/* <ParticlesBackground/> */}
        <Navbar/>
        <Home/>
        <About/>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,166 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo } from "react";
+import ParticlesBackground from "../components/ParticlesBackground";
+import { motion } from "framer-motion";
+import { FaXTwitter, FaLinkedin, FaGithub } from "react-icons/fa6";
+import avatar from "../assets/avatar.png";
 
 const Home = () => {
-  return (
-    <div className='w-full h-screen'>
-      home page
-    </div>
-  )
-}
+  const roles = useMemo(() => ["Web Developer", "Full Stack Developer"], []);
 
-export default Home
+  const [index, setIndex] = useState(0);
+  const [subIndex, setSubIndex] = useState(0);
+  const [deleting, setDeleting] = useState(false);
+
+  const socials = [
+    { Icon: FaXTwitter, label: "X", href: "https://x.com/gauravbuilds" },
+    { Icon: FaLinkedin, label: "LinkedIn", href: "https://linkedin.com/in/yourprofile" },
+    { Icon: FaGithub, label: "GitHub", href: "https://github.com/gauravguptasg64" },
+  ];
+
+  const glowVariants = {
+    initial: { scale: 1, y: 0, filter: "drop-shadow(0 0 0 rgba(0,0,0,0))" },
+    hover: {
+      scale: 1.2, y: -3,
+      filter: "drop-shadow(0 0 8px rgba(13,80,204,0.9)) drop-shadow(0 0 18px rgba(16,185,129,0.8))",
+      transition: { type: "spring", stiffness: 300, damping: 15 },
+    },
+    tap: { scale: 0.95, y: 0, transition: { duration: 0.08 } }
+  };
+
+  useEffect(() => {
+    const current = roles[index];
+    const timeout = setTimeout(
+      () => {
+        if (!deleting && subIndex < current.length) {
+          setSubIndex((v) => v + 1);
+        } else if (!deleting && subIndex == current.length) {
+          setTimeout(() => setDeleting(true), 1200);
+        } else if (deleting && subIndex > 0) {
+          setSubIndex((v) => v - 1);
+        } else if (deleting && subIndex == 0) {
+          setDeleting(false);
+          setIndex((p) => (p + 1) % roles.length);
+        }
+      },
+      deleting ? 40 : 60,
+    );
+
+    return () => clearTimeout(timeout);
+  }, [subIndex, index, deleting, roles]);
+  return (
+    <section
+      id="home"
+      className="w-full min-h-screen relative bg-black overflow-hidden pt-20"
+    >
+      <ParticlesBackground />
+      <div className="absolute inset-0">
+        <div
+          className="absolute -top-32 -left-32
+          w-[70vw] sm:w-[z-500vw] md:w-[40vw]
+          h-[70vw] sm:h-[z-500vw] md:h-[40vw]
+          max-w-[500px] max-h-[500px]
+          rounded-full
+          bg-gradient-to-r from-[#302b63] via-[#0038f8] to-[#1cd8d2]
+          opacity-30 sm:opacity-20 md:opacity-10
+          blur-[100px] sm:blur-[130px] md:blur-[150px]
+          animate-pulse"
+        />
+      </div>
+      <div
+        className="absolute bottom-0 right-0
+        w-[70vw] sm:w-[z-500vw] md:w-[40vw]
+        h-[70vw] sm:h-[z-500vw] md:h-[40vw]
+        max-w-[500px] max-h-[500px]
+        rounded-full
+        bg-gradient-to-r from-[#302b63] via-[#0038f8] to-[#1cd8d2]
+        opacity-30 sm:opacity-20 md:opacity-10
+        blur-[100px] sm:blur-[130px] md:blur-[150px]
+        animate-pulse delay-500"
+      />
+
+      <div className="relative z-10 w-full max-w-7xl mx-auto px-4 py-10 grid grid-cols-1 lg:grid-cols-2">
+        <div className="flex flex-col justify-center text-center lg:text-left relative">
+          <div className="w-full lg:pr-24 mx-auto max-w-[48rem]">
+            <motion.div
+              className="mb-3 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-white tracking-wide min-h-[1.6em]"
+              initial={{ opacity: 0, y: 12 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6 }}
+            >
+              <span>{roles[index].substring(0, subIndex)}</span>
+              <span
+                className="inline-block w-[2px] ml-1 bg-white animate-pulse align-middle"
+                style={{ height: "1em" }}
+              ></span>
+            </motion.div>
+            <motion.h1
+              className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#1cd8d2] via-[#0038f8] to-[#302b63] drop-shadow-lg"
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              Hello I'm
+              <br />
+              <span className="text-white font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl lg:whitespace-nowrap">
+                Sushant Prasad
+              </span>
+            </motion.h1>
+            <motion.p
+              className="mt-6 text-base sm:text-lg md:text-xl text-gray-300 max-w-2xl lg:min-0"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.4, duration: 0.8 }}
+            >
+              I turn complex ideas into seamless, high-impact web experiences –
+              building modern, scalable, and lightning-fast applications that
+              make a difference.
+            </motion.p>
+            <motion.div
+              className="mt-10   flex flex-wrap items-center justify-center lg:justify-start gap-6"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.8, duration: 0.8 }}
+            >
+              <a
+                href="#projects"
+                className="px-6 py-3 rounded-full font-medium text-lg text-white bg-gradient-to-r from-[#1cd8d2] via-[#00b8f8] to-[#302b63] shadow-lg hover:scale-105 transition-all "
+              >
+                View My Work
+              </a>
+              <a
+                href="/Resume.pdf"
+                download
+                className="px-6 py-3 rounded-full text-lg font-medium text-black bg-white hover:bg-gray-200 shadow-lg hover:scale-105 transition-all "
+              >
+                My Resume
+              </a>
+            </motion.div>
+            <motion.div className="mt-10 flex gap-5 text-2xl md:text-3xl justify-center lg:justify-start">
+              {socials.map(({ Icon, label, href }) => (
+                <motion.a
+                  href={href}
+                  key={label}
+                  target="_blank"
+                  aria-label={label}
+                  rel="noopener noreferrer"
+                  variants={glowVariants}
+                  initial="initial"
+                  whileHover="hover"
+                  whileTap="tap"
+                  className="text-gray-300 hover:text-white transition-color"
+                >
+                  <Icon/>
+                </motion.a>
+              ))}
+            </motion.div>
+          </div>
+        </div>
+        <div>
+          <motion.img src={avatar} alt="Sushant Prasad" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
